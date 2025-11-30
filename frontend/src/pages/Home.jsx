c4c9d956348f549104180a3e9b65d8a58b51e4b7
Bamidele Olaide <bammyoly@gmail.com>
2025-12-01 00:08:43 +0100

    Fix asset path and build Redot frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import HeroImageThumb from '../assets/Hero_thumb.png';
+import HeroImageThumb from '../assets/hero_thumb.png';
 import { Link } from 'react-router-dom'
 
 const Home = () => {
